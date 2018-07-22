19e7f6786d36ee4a82db29dfe9c583561dad217d
Max Gavrosh <mmg@anadeainc.com>
2018-07-22 12:00:47 +0300

    Update Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+export default class Layout extends Component {
+  state = {
+    sideDrawer: false,
+  }
 
-export default layout;
+  closeSideDrawer = () => this.setState({ sideDrawer: false });
+
+  toggleSideDrawer = () => this.setState({ sideDrawer: true });
+
+  render() {
+    return(
+      <Aux>
+        <Toolbar drawerToggleClicked={this.toggleSideDrawer} />
+        <SideDrawer closed={this.closeSideDrawer} open={this.state.sideDrawer} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+};
