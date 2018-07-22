0805bb2ac2201141eae2034939abfdb108b79f1a
Max Gavrosh <mmg@anadeainc.com>
2018-07-22 10:59:46 +0300

    Update BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -20,6 +22,7 @@ export default class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchasable: false,
+    purchasing: false,
   }
 
   updatePurchaseState(ingredients) {
@@ -54,12 +57,26 @@ export default class BurgerBuilder extends Component {
     this.updatePurchaseState(newIngredients)
   }
 
+  purchaseHandler= () => this.setState({ purchasing: true });
+
+  purchaseCancelHandler= () => this.setState({ purchasing: false });
+
+  purchaseContinueHandler= () => alert('You continue!');
+
   render() {
     const disabledInfo = { ...this.state.ingredients };
     Object.keys(disabledInfo).map(ingredient => disabledInfo[ingredient] = disabledInfo[ingredient] <= 0);
 
     return(
       <Aux>
+        <Modal show={this.state.purchasing} modalClose={this.purchaseCancelHandler}>
+          <OrderSummary
+            price={this.state.totalPrice}
+            purchaseCancelHandler={this.purchaseCancelHandler}
+            purchaseContinueHandler={this.purchaseContinueHandler}
+            ingredients={this.state.ingredients}
+          />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           price={this.state.totalPrice}
@@ -67,6 +84,7 @@ export default class BurgerBuilder extends Component {
           purchasable={this.state.purchasable}
           addIngredient={this.addIngredient}
           removeIngredient={this.removeIngredient}
+          purchaseHandler={this.purchaseHandler}
         />
       </Aux>
     )
